747efd1515a27ae240ed0fb5a1d5a7a0155ddc72
Jamie Binns <jbinns@five-startech.com>
2019-12-08 10:20:49 -0500

    WIP - added README, updated github link

---
+++ README.md
@@ -0,0 +1,6 @@
+# Canvas Demo
+
+This is a demo of the capabilities of the <canvas> element's 2D API. The examples show basic functionality, basic animation, graphs, and complex animations/games.
+
+### Live Demo
+[https://jamiecanvasdemo.herokuapp.com/](https://jamiecanvasdemo.herokuapp.com/)
--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Example5 from "examples/Example5.js";
 const App = () => (
     <div>
         <div>
-            <h3>This is a demonstration of the capabilities of the Canvas element. Most individual demos show a code snippet but for the full code please visit the <a href="https://github.com/Binnsler">Github</a> repo.</h3>
+            <h3>This is a demonstration of the capabilities of the Canvas element. Most individual demos show a code snippet but for the full code please visit the <a href="https://github.com/Binnsler/canvasdemo">Github</a> repo.</h3>
         </div>
         <Example1/>
         <Example2/>
